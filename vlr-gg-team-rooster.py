d93c7299341acecde4bf71fffe4000ae69e8d532
Bruno Correia <bruninhocorreia2013@gmail.com>
2023-03-28 17:28:26 -0300

    introducing team rating and name

--- vlr-gg-team-rooster.py
+++ vlr-gg-team-rooster.py
@@ -12,6 +12,17 @@ site = BeautifulSoup(response.content.decode('utf-8'), 'html.parser')
 player_name_list = site.find_all('div', attrs={'class':'team-roster-item-name-real'})
 player_nickname_list = site.find_all('div', attrs={'class':'team-roster-item-name-alias'})
 
+team_name = site.find('h1', attrs={'class':'wf-title'}).text
+team_rating = site.find('div', attrs={'class':'rating-num'}).text.strip()
+team_wins = site.find('div', attrs={'class':'win'})
+team_losses = site.find('div', attrs={'class':'loss'})
+
+print(team_name)
+print('Team Rating: ' + team_rating)
+print('____________________________________')
+print()
+
+
 for name, nickname in zip(player_name_list, player_nickname_list):
     player_name = name.text.strip().encode('utf-8').decode('unicode_escape')
     player_nickname = nickname.text.strip().encode('utf-8').decode('unicode_escape')
