431a4c77b3ead6821d7382838fe05ec5577d3baf
Bruno Correia <bruninhocorreia2013@gmail.com>
2023-03-29 13:01:10 -0300

    match header

--- vlr-gg-match-result.py
+++ vlr-gg-match-result.py
@@ -6,9 +6,19 @@ url_player = 'https://www.vlr.gg/184649/team-liquid-brazil-vs-loud-gc-game-chang
 headers = {'user-agent':'Mozilla/5.0'}
 
 response = requests.get(url_player, headers = headers)
-
 site = BeautifulSoup(response.content.decode('utf-8'), 'html.parser')
 
+
+# Getting event and match info (event name) #
+event_info = site.find('div', attrs={'class':'match-header-super'})
+event_name = event_info.find('div', attrs={'style':'font-weight: 700;'}).text.strip()
+
+print(event_name)
+
+# Getting match score #
+score_param = site.find('span', attrs={'class':'match-header-vs-score-colon'})
+score_team_a = score_param.find_previous().text.strip()
+score_team_b = score_param.find_next().text.strip()
 
 # Getting Team A (right side) info #
 team_a_info = site.find('div', attrs={'class':'match-header-link-name mod-1'})
@@ -22,6 +32,12 @@ team_b_info = site.find('div', attrs={'class':'match-header-link-name mod-2'})
 team_b_name = team_b_info.find('div', attrs={'class':'wf-title-med'}).text.strip()
 team_b_rating = team_b_info.find('div', attrs={'class':'match-header-link-name-elo'}).text.strip()
 
-print(team_a_name, team_a_rating)
-print(team_b_name, team_b_rating)
+# Printing match header with name, rating and score #
+print(team_a_name, team_a_rating, score_team_a + ' x ' + score_team_b, team_b_rating, team_b_name)
+
+# Getting Picks and Bans info #
+
+
+
+
 
